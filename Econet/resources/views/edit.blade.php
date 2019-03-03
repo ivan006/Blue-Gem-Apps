f6e1f54fb0e9354ca12ea7ecd1fd66ac885018a1
ivan006 <ivan.copeland2015@gmail.com>
2019-03-03 20:42:12 +0200

    1

--- Econet/resources/views/edit.blade.php
+++ Econet/resources/views/edit.blade.php
@@ -61,10 +61,10 @@
 
 <a href="{{ URL::asset($VPgLocMode1) }}">view</a>
 <br>
-<h1>Rich Data</h1>
 
 
  <form class="" action="{{ URL::asset($VPgLocMode1)}}" method="post">
+   <h1>Rich Data</h1>
    <!-- surname 1: [r]Education/Destiny Code/smart/surname.txt[/r]<br>  surname 2: [r]Education/Graft Your Garden/smart/surname.txt[/r]<br>      -->
 
    {{csrf_field()}}
@@ -76,13 +76,14 @@
      }
      ?></textarea>
    <input style="display: none;" type="text"  name="file" value="<?php echo $rich; ?>"  placeholder="Enter title">
-   <textarea style="display: none;"  id="theLord" type="text" name="smart" value=""></textarea>
+   <h1>Smart Data</h1>
+   <textarea style="display: nnone;"  id="theLord" type="text" name="smart" value=""></textarea>
 
 
  </form>
 
 <br>
-<h1>Smart Data</h1>
+
 
 <div class="json-editor" id="mydiv"></div>
 
@@ -91,35 +92,42 @@
 <script src="{{ URL::asset('js/FlexiJsonEditor/jquery.jsoneditor.js') }}"></script>
 
 <script type="text/javascript">
-
-
-<?php
-if (isset($VPgCont['smart'])) {
-  $ivan3 = array();
-  $ivan3["smart"] = $VPgCont['smart'];
-  $ivan_json =  json_encode($ivan3);
-} else {
-  $ivan_json =  null;
-
-}
-?>
-var myjson = <?php echo $ivan_json; ?>;
-var opt = {
-  change: function(data) { /* called on every change */ },
-  propertyclick: function(path) { /* called when a property is clicked with the JS path to that property */ }
-};
-/* opt.propertyElement = '<textarea>'; */ // element of the property field, <input> is default
-/* opt.valueElement = '<textarea>'; */  // element of the value field, <input> is default
-$('#mydiv').jsonEditor(myjson, opt);
+// <?php
+// if (isset($VPgCont['smart'])) {
+//   $ivan3 = array();
+//   $ivan3["smart"] = $VPgCont['smart'];
+//   $ivan_json =  json_encode($ivan3);
+// } else {
+//   $ivan_json =  null;
+// }
+// ?>
+// var myjson =
+// <?php
+// echo $ivan_json;
+// ?>
+// ;
+// var opt = {
+//   change: function(data) { /* called on every change */ },
+//   propertyclick: function(path) { /* called when a property is clicked with the JS path to that property */ }
+// };
+// /* opt.propertyElement = '<textarea>'; */ // element of the property field, <input> is default
+// /* opt.valueElement = '<textarea>'; */  // element of the value field, <input> is default
+// $('#mydiv').jsonEditor(myjson, opt);
 </script>
 
-<script type="text/javascript">
-  // let thingydo = document.getElementById("myDiv").getElementsByTagName("DIV")[0].getElementsByClassName("value")[0];
+
+<!-- <script type="text/javascript">
+
   let thingydo = document.getElementById("mydiv").getElementsByTagName("DIV")[0].getElementsByClassName("value")[0];
   thingydo.onkeyup = function() {myFunctiondd()};
 
-  function myFunctiondd() {
 
-    document.getElementById("theLord").value = thingydo.value;
-  }
+  function myFunctiondd() {  document.getElementById("theLord").value = thingydo.value;}
+
+</script> -->
+<script type="text/javascript">
+// document.getElementById("theLord").value = myjson.smart.toSource();
+// document.getElementById("theLord").value = JSON.stringify(myjson.smart);
+document.getElementById("theLord").value = JSON.stringify(myjson);
+
 </script>
