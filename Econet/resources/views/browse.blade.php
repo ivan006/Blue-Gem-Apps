b19061814975ee5c6a3365792b43efca1bac0125
ivan006 <ivan.copeland2015@gmail.com>
2019-05-17 21:09:51 +0200

    1

---
+++ Econet/resources/views/browse.blade.php
@@ -0,0 +1,19 @@
+
+
+
+
+<br>
+<br>
+<div class="" style="background-color: white; padding: 0em; width: 100%; height: 200px;">
+
+  <?php
+  if (isset($VSiteHeader)) {
+    echo $VSiteHeader;
+  }
+
+  if (isset($VPgCont["rich.txt"])) {
+    echo $VPgCont["rich.txt"];
+  }
+  ?>
+
+</div>
