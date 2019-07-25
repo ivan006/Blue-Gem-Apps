f8b3fe0b636d6618b8733d5f987ef07216c6b493
ivan006 <ivan.copeland2015@gmail.com>
2019-07-25 16:50:41 +0200

    1

--- resources/views/includes/general-include-one-of-four.blade.php
+++ resources/views/includes/general-include-one-of-four.blade.php
@@ -6,6 +6,6 @@
   <title>Harmonyville - One doc to rule them all and in the darkness bind them</title>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link href="{{ asset('public/css/w3.css') }}" rel="stylesheet">
+  <link href="{{ asset('css/w3.css') }}" rel="stylesheet">
 
-  <link href="{{ asset('public/css/w3-theme-blue-grey.css') }}" rel="stylesheet">
+  <link href="{{ asset('css/w3-theme-blue-grey.css') }}" rel="stylesheet">
